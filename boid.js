9d6d08b522d2bc2525e22372ea03d761f2afc1d5
James Plimmer <sc21jpwp@leeds.ac.uk>
2024-08-12 12:42:33 +0100

    added cohesion

--- boid.js
+++ boid.js
@@ -5,9 +5,10 @@ class Boid {
     this.velocity.setMag(random(2, 4));
     this.acceleration = createVector();
     this.maxForce = 0.2;
-    this.maxSpeed = 2;
+    this.maxSpeed = 4;
   }
 
+  // wrap around edges of canvas
   edges() {
     if (this.position.x > width) {
       this.position.x = 0;
@@ -22,8 +23,8 @@ class Boid {
     }
   }
 
-  align(flock) {
-    let perceptionRadius = 50;
+  alignOrCohere(flock, mode) {
+    let perceptionRadius = 100;
     let steeringForce = createVector();
     let boidsInRadius = 0;
     for (let other of flock) {
@@ -34,29 +35,43 @@ class Boid {
         other.position.y
       );
       if (other != this && d < perceptionRadius) {
-        steeringForce.add(other.velocity);
         boidsInRadius++;
+        if (mode === 'align') {
+          // find average velocity to move flock in same direction
+          steeringForce.add(other.velocity);
+        }
+        if (mode === 'cohere') {
+          // find average position to make boids travel towards center of flock
+          steeringForce.add(other.position);
+        }
       }
     }
 
     if (boidsInRadius > 0) {
-      steeringForce.div(boidsInRadius);
-      steeringForce.sub(this.velocity);
-      steeringForce.limit(this.maxForce);
+      steeringForce.div(boidsInRadius); // divide to get average
+      if (mode === 'cohere') {
+        steeringForce.sub(this.position); // subtract current position to get vector pointing towards average position
+      }
       steeringForce.setMag(this.maxSpeed);
+      steeringForce.sub(this.velocity); // subtract current velocity to get steering force needed to correct current velocity
+      steeringForce.limit(this.maxForce);
     }
 
     return steeringForce;
   }
 
   flock(boids) {
-    let alignment = this.align(boids);
-    this.acceleration = alignment;
+    let alignment = this.alignOrCohere(boids, 'align');
+    let cohesion = this.alignOrCohere(boids, 'cohere');
+    this.acceleration.add(alignment);
+    this.acceleration.add(cohesion);
   }
 
   update() {
     this.position.add(this.velocity);
     this.velocity.add(this.acceleration);
+    this.velocity.limit(this.maxSpeed);
+    this.acceleration.mult(0); // reset acceleration before next update (preventing accumulation)
   }
 
   show() {
